5d8ca2ef45733b6015d961abbf90ba18b35e3e11
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-25 08:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 //complete this code
-class Rectangle {
-	constructor(width, height){
-		this._width = width;
-		this._height = height;
+class Person {
+	constructor(name, age){
+		this.name=name,
+		this.age=age
+	}
+	get : name(){
+		return this.name;
+	}
+	get : age(){
+		return this.age;
 	}
 
-	get width(){
-		return this._width;
-	}
-
-	get height(){
-		return this._height;
-	}
-
-	getArea(){
-		return this.width * this.height;
+	set : age(age1){
+		this.age=age1
 	}
 }
 
-class Square extends Rectangle {
-	constructor(side){
-		super(side, side);
-	}
-	
-	getPerimeter(){
-		return 4 * this.width;
+class Student extends Person {
+	// constructor(name){
+	// 	super(name);
+		
+	// }
+	study(){
+		console.log(`${this.name} is studying`)
 	}
 }
 
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.width);
-console.log(rectangle.height);
-console.log(rectangle.getArea());
+class Teacher extends Person {
+	// constructor(name){
+	// 	super(name)
+	// }
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+// 
+const person = new Person("Jhon, 25");
+console.log(person.name);
+person.age = 30;
+console.log(person.age);
 
-const square = new Square(7);
-console.log(square.width);
-console.log(square.height);
-console.log(square.getArea());
-console.log(square.getPerimeter());
+const student = new Student("Alice, 22");
+student.study();
+
+const teacher = new Teacher("Bob, 40");
+teacher.teach();
+
 
 // Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
