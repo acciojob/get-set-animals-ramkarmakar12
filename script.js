7ee8b9de45fb81c8f8f98f38350fc7b80a35a5fd
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-25 08:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,40 @@
 //complete this code
-class Person {
-	constructor(name, age){
-		this.name=name,
-		this.age=age
-	}
-	get : name(){
-		return this.name;
-	}
-	get : age(){
-		return this.age;
+class Animal {
+
+	constructor(species){
+		this._species = species;
 	}
 
-	set : age(age1){
-		this.age=age1
+	get species(){
+		return this._species;
+	}
+
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`);
 	}
 }
 
-class Student extends Person {
-	// constructor(name){
-	// 	super(name);
-		
-	// }
-	study(){
-		console.log(`${this.name} is studying`)
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
 	}
 }
 
-class Teacher extends Person {
-	// constructor(name){
-	// 	super(name)
-	// }
-	teach(){
-		console.log(`${this.name} is teaching`)
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
 	}
 }
-// 
-const person = new Person("Jhon, 25");
-console.log(person.name);
-person.age = 30;
-console.log(person.age);
 
-const student = new Student("Alice, 22");
-student.study();
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+myCat.purr();
 
-const teacher = new Teacher("Bob, 40");
-teacher.teach();
-
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+myDog.bark();
 
 // Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
